7509fa5478f56a0157e6315fb468aff33a8e9e39
EricaSOS <erica.sos.adv@hotmail.com>
2026-04-07 20:51:49 -0300

    fix: correção da chamada do banco de dados, para modelo singleton, ou seja, uma chamada só e utilização para todo o projeto.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,17 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
 
-export async function openDb() {
-  return open({
-    filename: "./database.sqlite",
-    driver: sqlite3.Database
-  });
+let dbInstance: Database | null = null;
+
+export async function getDb() {
+  if (!dbInstance) {
+    dbInstance = await open({
+      filename: "./database.sqlite",
+      driver: sqlite3.Database
+    });
+
+    console.log("Conexão com banco criada.");
+  }
+
+  return dbInstance;
 }
