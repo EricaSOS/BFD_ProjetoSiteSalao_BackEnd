b0e5ce718918bac7a20263afa95307c9f7e81405
EricaSOS <erica.sos.adv@hotmail.com>
2026-04-07 22:53:50 -0300

    feat: inclusão de nova rota para listar profissional por serviço

--- src/routes/servicesRoutes.ts
+++ src/routes/servicesRoutes.ts
@@ -7,9 +7,13 @@ import {
   deleteService
 } from "../controllers/servicesController.js";
 
+import { listProfessionalsByService } from "../controllers/professionalsController.js";
+
+
 const router = Router();
 
 router.get("/", listServices);
+router.get("/:id/professionals", listProfessionalsByService);
 router.get("/:id", getServiceById);
 router.post("/", createService);
 router.patch("/:id", updateService);
