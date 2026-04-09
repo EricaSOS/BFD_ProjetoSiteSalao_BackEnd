d216c8c89d7d8b36facb8f335f2064a53b26f5de
EricaSOS <erica.sos.adv@hotmail.com>
2026-04-08 23:01:48 -0300

    feat: correção da ordem dos dias da semana, afetou dateUtils e professionalScheduleSeed

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -3,5 +3,5 @@ export function getDayOfWeekFromDate(date: string): number {
   const utcDate = new Date(Date.UTC(year, month - 1, day));
   const dayOfWeek = utcDate.getUTCDay();
 
-  return dayOfWeek === 0 ? 7 : dayOfWeek;
+  return dayOfWeek + 1;
 }
