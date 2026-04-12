f4d41cc93850e5cd3258f33ab7de45c66c615a3f
EricaSOS <erica.sos.adv@hotmail.com>
2026-04-12 00:47:16 -0300

    feat: inclusão dos comando relativos ao swagger no server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,15 @@ import servicesRoutes from "./routes/servicesRoutes.js";
 import professionalsRoutes from "./routes/professionalsRoutes.js";
 import appointmentsRoutes from "./routes/appointmentsRoutes.js";
 
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./docs/swagger.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
